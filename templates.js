91360634970f600b8d5adb5934928d2eaca19982
Tyler Cecchi <thcecchi@gmail.com>
2015-02-02 09:46:57 -0500

    minor design revisions

--- templates.js
+++ templates.js
@@ -4,7 +4,7 @@ templates.farmResults = [
   "<h2>Farm:</h2><h5><%= farm %></h5>",
   "<h2>Price:</h2><h5><%= price %></h5>",
   "<h2>Pickup Location:</h2><h5><%= pickup %></h5>",
-  "<h2>Learn More:</h2><h5><a><%= url %></a></h5>"
+  "<h2>Learn More:</h2><h5 class='url'><a><%= url %></a></h5>"
  ].join("")
 
 
